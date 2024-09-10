b9f54a55a004c98eb4b120c12263ce7622050424
Muhammadrasul21 <theboy6878@gmail.com>
2024-09-11 02:23:25 +0500

    edited js

--- app.js
+++ app.js
@@ -111,7 +111,119 @@ console.log("End");
 
 
 
-{
-    // 5 masala 
+// {
+//     // 5 masala 
 
-}
+//     function isFibo(a, b, f) {
+//         if (f === a || f === b) return true;
+
+//         while (b <= f) {
+//             let c = a + b;
+//             if (c === f) return true;
+//             a = b;
+//             b = c;
+//         }
+
+//         return false;
+//     }
+// }
+
+
+
+
+// {
+//     // 6 masala
+//     function getCount(str) {
+//         const vowels = 'aeiou';
+//         let count = 0;
+
+//         for (let char of str) {
+//             if (vowels.includes(char)) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+// {
+//     // 7 masala
+//     function keysAndValues(data) {
+
+//         const keys = Object.keys(data);
+//         const values = Object.values(data);
+
+//         return [keys, values];
+//     }
+// }
+
+
+
+
+
+
+// {
+//     // 8 masala
+//     function disemvowel(str) {
+
+//         const vowels = 'aeiou';
+
+//         return str.split('').filter(char => !vowels.includes(char.toLowerCase())).join('');
+//     }
+// }
+
+
+
+
+
+// {
+//     // 9 masala
+//     function sameCase(a, b) {
+//         const isLetter = char => /^[a-zA-Z]$/.test(char);
+
+//         if (!isLetter(a) || !isLetter(b)) {
+//             return -1;
+//         }
+
+//         if ((a === a.toLowerCase() && b === b.toLowerCase()) ||
+//             (a === a.toUpperCase() && b === b.toUpperCase())) {
+//             return 1;
+//         }
+
+//         return 0;
+//     }
+// }
+
+
+
+
+
+// {
+//     // 10 masala 
+//     function greet(language) {
+//         const greetings = {
+//             "english": "Welcome",
+//             "czech": "Vitejte",
+//             "danish": "Velkomst",
+//             "dutch": "Welkom",
+//             "estonian": "Tere tulemast",
+//             "finnish": "Tervetuloa",
+//             "flemish": "Welgekomen",
+//             "french": "Bienvenue",
+//             "german": "Willkommen",
+//             "irish": "Failte",
+//             "italian": "Benvenuto",
+//             "latvian": "Gaidits",
+//             "lithuanian": "Laukiamas",
+//             "polish": "Witamy",
+//             "spanish": "Bienvenido",
+//             "swedish": "Valkommen",
+//             "welsh": "Croeso"
+//         };
+
+//         return greetings[language.toLowerCase()] || "Welcome";
+//     }
+// }
